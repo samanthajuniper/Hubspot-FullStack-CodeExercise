5bf49f9476d517994aec5e2b10c21ac6904dd638
David <2527460+david-mooney@users.noreply.github.com>
2024-02-02 15:42:21 +0000

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const port = process.env.SERVER_PORT || 3001 ;
 const mediaRouter = require('./routes/media');
+const port = process.env.SERVER_PORT || 3001 ;
 
 app.get('/', (_, res) => {
   res.json({ message: 'Hello World' });
